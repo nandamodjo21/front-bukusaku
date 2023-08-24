cb9c158089322967516d2b0a8bb0367e37a51d35
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-08-24 11:47:46 +0800

    fix: update function home

--- app/src/main/java/com/example/buku_saku/home/HomesActivity.java
+++ app/src/main/java/com/example/buku_saku/home/HomesActivity.java
@@ -5,14 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,27 +15,21 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.buku_saku.POJO.FileData;
-import com.example.buku_saku.POJO.Materi;
 import com.example.buku_saku.R;
-import com.example.buku_saku.adapter.MateriArrayAdapter;
 import com.example.buku_saku.koneksi.ApiConnect;
-import com.example.buku_saku.menu.Menus;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 public class HomesActivity extends AppCompatActivity {
 
-    private ImageView list;
-    private ListView ls;
-    private TextView notfound;
-    private ArrayList<Materi> arrayList;
-    private MateriArrayAdapter adapter;
+    private ImageView image;
+    private String pdApiUrl;
+    private TextView materi;
+
 
 
     @SuppressLint("MissingInflatedId")
@@ -50,16 +38,17 @@ public class HomesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homes);
 
-        notfound = findViewById(R.id.not);
-        ls = findViewById(R.id.listView);
-        list = findViewById(R.id.mn);
-        arrayList = new ArrayList<>();
-        adapter = new MateriArrayAdapter(this,arrayList);
-        ls.setAdapter(adapter);
+        image = findViewById(R.id.imagePdg);
+        materi = findViewById(R.id.textMateri);
+
+
+
+
 
 
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+
 
 
 
@@ -71,26 +60,15 @@ public class HomesActivity extends AppCompatActivity {
                         JSONObject jsonObject = response.getJSONObject(i);
 
                         String id = jsonObject.getString("id_soal");
-                        String materi = jsonObject.getString("materi");
+                        String nama_materi = jsonObject.getString("materi");
                         String fileMateri = jsonObject.getString("file_materi");
                         String soal = jsonObject.getString("soal");
 
-                        FileData fileData = new FileData(fileMateri,soal,id);
-                        String ms = "not found";
-                        System.out.println(materi);
-                        Materi dataMateri = new Materi(fileData);
+                        materi.setText(nama_materi);
 
-                        if (dataMateri != null){
-                            arrayList.add(dataMateri);
-                        } else {
-                            notfound.setText("NOT found");
-                        }
-
-
-
+                        pdApiUrl = ApiConnect.url_download + "?filename=" + fileMateri;
 
                     }
-                    adapter.notifyDataSetChanged();
                 }catch (JSONException e){
                     e.printStackTrace();
                 }
@@ -103,37 +81,34 @@ public class HomesActivity extends AppCompatActivity {
 
             }
         }); requestQueue.add(jsonArrayRequest);
-
-        System.out.println(jsonArrayRequest);
-
-
-        list.setOnClickListener(new View.OnClickListener() {
+        image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Menus.class));
-                finish();
-            }
-        });
-
-        ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-              Materi pilih = arrayList.get(position);
-//              String materi = pilih.getMateri();
-              String fileMateri = pilih.getFileData().getFileName();
-//                String idString = String.valueOf(pilih.getId());
-                String soal = pilih.getFileData().getSoal();
-                String soalId = pilih.getFileData().getIdSoal();
-
-
-                Intent intent = new Intent(getApplicationContext(), Uji.class);
-//                intent.putExtra("ID",idString);
-//                intent.putExtra("MATERI", materi);
-                intent.putExtra("FILE",fileMateri);
-                intent.putExtra("SOAL", soal );
-                intent.putExtra("ID",soalId);
-                startActivity(intent);
+              openPDf(pdApiUrl);
             }
         });
     }
+
+    private void openPDf(String pdApiUrl) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, pdApiUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+              Intent intent = new Intent(getApplicationContext(), Pdf.class);
+              intent.putExtra("data", pdApiUrl);
+              startActivity(intent);
+              finish();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this); requestQueue.add(stringRequest);
+    }
+
+
 }
